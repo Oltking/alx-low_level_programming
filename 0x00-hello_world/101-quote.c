a2bcefe23bcacbdc725b3b0558a72d69b4cb1e6a
Oladiji Olamide <olamideoladiji600@gmail.com>
2023-03-16 19:04:45 -0700

    101-quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,8 @@
  * Return: Return 1 after success
  */
 int main(void)
+
 {
-	fput("and that piece of art is useful\" - Dora Korpor, 2015-10-19\n", stdout);
+	fputs("and that piece of art is useful\" - Dora Korpor, 2015-10-19\n", stdout);
 	return (1);
 }
