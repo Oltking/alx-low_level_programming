9c166d2d70ec6a88cd602b80f6a2c11557bc7ac5
Oladiji Olamide <olamideoladiji600@gmail.com>
2023-03-16 18:46:51 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,9 +5,8 @@
  * main - Entry point pf a c programme
  * Return: Return 1 after success
  */
-
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpor, 2015-10-19\n", stdout);
+	fput("and that piece of art is useful\" - Dora Korpor, 2015-10-19\n", stdout);
 	return (1);
 }
