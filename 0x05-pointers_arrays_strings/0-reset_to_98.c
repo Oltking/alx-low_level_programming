dc7b5133a2248361b84ae470e2e81d714a5abcd6
Oladiji Olamide <olamideoladiji600@gmail.com>
2023-03-27 10:36:27 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - to check the code in c programming
+ *
+ * _putchar - output
+ *
+ * Return: return (0) if successful
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
