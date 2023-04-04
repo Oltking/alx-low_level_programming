e7e46b8924b09d046864fa9f9e2f7142645ade32
Oladiji Olamide <olamideoladiji600@gmail.com>
2023-04-04 14:36:37 -0700

    100-set_string.c

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * set_string - function to set pointer to a string
+ *
+ * @s: param for pointer
+ * @to: param for pointed value
+ *
+ * Return: return 0 if successful
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
