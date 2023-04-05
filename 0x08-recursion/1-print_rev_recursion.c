37159fd0cadf0a160399c5ed2a5e856bb6635126
Oladiji Olamide <olamideoladiji600@gmail.com>
2023-04-05 16:02:30 -0700

    1-print_rev_recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - function to print string in reverse
+ *
+ * @s: parameter pointer for char
+ *
+ * Return: nothing to return
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
