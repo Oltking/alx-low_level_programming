0cc05cd2036ed5f6cc958a61a57f54209044dead
Oladiji Olamide <olamideoladiji600@gmail.com>
2023-04-17 19:54:53 -0700

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - function to print the name of the file
+ *
+ * Return: always return (0) on  success
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
